b7dc6ab8a3aaa38f817f1b3a940581fb910dc981
Huang, Yun <huangyun.ai@gmail.com>
2014-08-12 19:16:17 +0800

    remove

--- FAST_v1.0/src/edu/berkeley/nlp/math/DifferentiableFunction.java
+++
@@ -1,7 +0,0 @@
-package edu.berkeley.nlp.math;
-
-/**
- */
-public interface DifferentiableFunction extends Function {
-  double[] derivativeAt(double[] x);
-}
--- FAST_v1.0/src/edu/berkeley/nlp/math/GradientLineSearcher.java
+++
@@ -1,8 +0,0 @@
-package edu.berkeley.nlp.math;
-
-/**
- * @author Dan Klein
- */
-public interface GradientLineSearcher {
-  public double[] minimize(DifferentiableFunction function, double[] initial, double[] direction);
-}
