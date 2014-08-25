d98429b62fff941a1324ccfd780f012cdbdf97e8
Huang, Yun <huangyun.ai@gmail.com>
2014-08-25 13:39:35 -0400

    remove

--- src/edu/berkeley/nlp/math/GradientMinimizer.java
+++
@@ -1,9 +0,0 @@
-package edu.berkeley.nlp.math;
-
-/**
- * @author Dan Klein
- */
-public interface GradientMinimizer {
-  double[] minimize(DifferentiableFunction function, double[] initial, double tolerance);
-  double[] minimize(DifferentiableFunction function, double[] initial, double tolerance, boolean project);
-}
